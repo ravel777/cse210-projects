c81967b391e84df0f3afa83dec088d24270c39cf
Ravel BITOUNGUI <bit23001@byui.edu>
2024-11-13 23:33:30 +0100

    Prep1 Done!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Enter your firstName: ");
+        string fname = Console.ReadLine();
+        Console.WriteLine("Enter your lasttName: ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your full name is: {fname} {lname}");
     }
 }
